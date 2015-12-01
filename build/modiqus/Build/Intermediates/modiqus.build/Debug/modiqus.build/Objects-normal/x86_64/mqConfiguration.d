e0783e191ab3720f1dd34508bec6f5c4028ff265
zollenz <dejean75@gmail.com>
2015-12-01 23:49:00 +0100

    Removed build folder from version control

--- build/modiqus/Build/Intermediates/modiqus.build/Debug/modiqus.build/Objects-normal/x86_64/mqConfiguration.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/dejean/Developer/projects/c++/modiqus/modiqus/src/mqConfiguration.cpp \
-  /Users/dejean/Developer/projects/c++/modiqus/modiqus/inc/mq_types.h \
-  /Users/dejean/Developer/projects/c++/modiqus/modiqus/inc/mqConfiguration.h
