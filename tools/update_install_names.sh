19f2b802c6e52b4e8a7095839201fafac2205a26
zollenz <dejean75@gmail.com>
2018-08-08 22:09:41 +0200

    Fix post-build script. Remove Csound64 linkage (get rid of warning, not needed for static lib).

--- tools/update_install_names.sh
+++ tools/update_install_names.sh
@@ -1,11 +1,35 @@
-cd ../build/modiqus/Build/Products/Debug
-# install_name_tool -change /Users/dejean/Developer/libraries/csound/build/Debug/CsoundLib.framework/Versions/6.0/CsoundLib @executable_path/lib/CsoundLibd.framework/Versions/6.0/CsoundLib libmodiqd.dylib
-cd ../Release
-# install_name_tool -change /Users/dejean/Developer/libraries/csound/build/Debug/CsoundLib.framework/Versions/6.0/CsoundLib @executable_path/lib/CsoundLib.framework/Versions/6.0/CsoundLib libmodiq.dylib
-cd ../../../../../ext/csound/debug/CsoundLib.framework
-install_name_tool -id @executable_path/lib/CsoundLib.framework/Versions/6.0/CsoundLib CsoundLib
-install_name_tool -change /usr/local/lib/libsndfile.1.dylib @executable_path/lib/libsndfile.1.dylib CsoundLib
-cd ../../release/CsoundLib.framework
-install_name_tool -id @executable_path/lib/CsoundLib.framework/Versions/6.0/CsoundLib CsoundLib
-install_name_tool -change /usr/local/lib/libsndfile.1.dylib @executable_path/lib/libsndfile.1.dylib CsoundLib
+#/bin/sh
+cd $1
+
+SNDFILE_REGEX='.*libsndfile.1.dylib\>'
+
+LIB_PATH=lib
+
+if [ "$2" = "bundle" ]; then
+	LIB_PATH=../Frameworks
+fi	
+
+# Update CsoundLib
+LIBSNDFILE_PATH=$(otool -L CsoundLib64 | grep -o $SNDFILE_REGEX)
+echo sndfile path : $LIBSNDFILE_PATH
+install_name_tool -id @executable_path/$LIB_PATH/CsoundLib64 CsoundLib64
+install_name_tool -change $LIBSNDFILE_PATH @executable_path/$LIB_PATH/libsndfile.1.dylib CsoundLib64
+otool -L CsoundLib64
+
+# Update dependencies
+for i in *.dylib; do
+	echo Processing $i
+	install_name_tool -id @executable_path/$LIB_PATH/$i $i
+
+	# Update sndfile dependency if exists and lib is not sndfile itself
+	if [ "$i" != "libsndfile.1.dylib" ]; then
+		LIBSNDFILE_PATH=$(otool -L $i | grep -o $SNDFILE_REGEX)
+
+		if [ "$LIBSNDFILE_PATH" != "" ]; then
+			echo sndfile path : $LIBSNDFILE_PATH
+			install_name_tool -change $LIBSNDFILE_PATH @executable_path/$LIB_PATH/libsndfile.1.dylib $i
+		fi
+	fi
 	
+	otool -L $i
+done
